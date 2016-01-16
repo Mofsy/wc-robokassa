3c679c46b16739639111994d1676e2255f990992
Mofsy <vptuhrcOmlCPRhhK9k8M>
2016-01-16 14:37:57 +0300

    Update logger

--- class-wc-robokassa-logger.php
+++ class-wc-robokassa-logger.php
@@ -155,8 +155,8 @@ class WC_Robokassa_Logger
         $content = array
         (
             $level,
-            $this->levels[$level],
             $this->dt->format(DATE_ATOM),
+            $this->levels[$level],
             $message
         );
 
@@ -168,7 +168,7 @@ class WC_Robokassa_Logger
         /**
          * Content
          */
-        $content = implode(' -|||- ', $content);
+        $content = implode(' -|- ', $content);
 
         file_put_contents
         (
