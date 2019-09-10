72d019b72d8741e8f4c1524ba3eb4ed1205002ba
Oleg Budrin <ru.mofsy@yandex.ru>
2019-09-10 19:41:07 +0300

    Fix icon url

--- includes/class-wc-robokassa-method.php
+++ includes/class-wc-robokassa-method.php
@@ -394,7 +394,7 @@ class Wc_Robokassa_Method extends WC_Payment_Gateway
 		 */
 		if($this->get_option('enable_icon') === 'yes')
 		{
-			$this->icon = apply_filters('woocommerce_robokassa_icon', WC_ROBOKASSA_URL . '/assets/img/robokassa.png');
+			$this->icon = apply_filters('woocommerce_robokassa_icon', WC_ROBOKASSA_URL . 'assets/img/robokassa.png');
 		}
 
 		/**
