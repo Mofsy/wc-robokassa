5331542ef531756e3cd070f3928762bdb216be69
Oleg Budrin <ru.mofsy@yandex.ru>
2020-01-24 19:40:06 +0300

    Fix: more

--- wc-robokassa.php
+++ wc-robokassa.php
@@ -10,7 +10,7 @@
  * Domain Path: /languages
  * Author: Mofsy
  * Author URI: https://mofsy.ru
- * Copyright: © 2015-2020 Mofsy
+ * Copyright: Mofsy © 2015-2020
  * License: GNU General Public License v3.0
  * License URI: http://www.gnu.org/licenses/gpl-3.0.html
  *
